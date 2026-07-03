35251789f9e90aed4bbe9acc85d87e301d410391
hanzala_safi <hanzalasafi9534@gmail.com>
2026-07-03 21:46:43 +0530

    week4 task1 main.py

---
+++ week4/task1/main.py
@@ -0,0 +1,14 @@
+# main.py
+
+import calculator
+
+print("=== Simple Calculator using Module ===")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\nResults:")
+print("Addition:", calculator.add(num1, num2))
+print("Subtraction:", calculator.subtract(num1, num2))
+print("Multiplication:", calculator.multiply(num1, num2))
+print("Division:", calculator.divide(num1, num2))
